c251fd82a9a752351bf464b0c85bb3ae8e9c4fca
divyanipatil093 <divyanipatil093@gmail.com>
2026-08-25 09:47:57 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -11,7 +11,7 @@ export const googleAuth = async (req, res) => {
         }
         let token = await genToken(user._id)
         res.cookie("token", token, {
-            http:true,
+            httpOnly:true,
             secure:true,
             semiSite:"none",
             maxAge: 7 * 24 * 60 * 60 * 1000
